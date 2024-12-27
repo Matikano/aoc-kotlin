f78149f0a887ae822fe4fd392638108bd0bc30ad
Matikano <56170622+Matikano@users.noreply.github.com>
2024-12-27 15:34:54 +0100

    Aoc2023 Day 10

--- src/aoc2023/day_10/Day10.kt
+++ src/aoc2023/day_10/Day10.kt
@@ -7,6 +7,7 @@ import kotlin.time.measureTime
 object Day10: AocTask() {
 
     override fun executeTask() {
+        // Test part
         with(PipeMaze(testInput.toGrid())) {
             solve()
             val numberOfEnclosedPositions = enclosedByLoopCount()
@@ -14,14 +15,16 @@ object Day10: AocTask() {
         }
 
         with (PipeMaze(inputToString().toGrid())) {
+            // Part 1
             measureTime {
                 solve()
                 println("Distance to furthest pipe from the start = $longestDistanceFromStart")
             }.let { println("Part one took $it\n") }
 
+            // Part 2
             measureTime {
                 println("Number of enclosed positions = ${enclosedByLoopCount()}")
-            }
+            }.let { println("Part 2 took $it\n") }
         }
     }
 }
