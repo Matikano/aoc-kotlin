157e03da932da801355528300c7191dc2af89739
Matikano <56170622+Matikano@users.noreply.github.com>
2024-12-28 13:42:56 +0100

    Aoc2023 Day 15

--- src/aoc2023/day_15/Day15.kt
+++ src/aoc2023/day_15/Day15.kt
@@ -10,30 +10,27 @@ object Day15: AocTask() {
 
     override fun executeTask() {
 
-
         measureTime {
             val boxes: MutableMap<Int, MutableList<Lens>> =
-                (0..MODULO_FACTOR).associate { it to mutableListOf<Lens>() }.toMutableMap()
+                (0..< MODULO_FACTOR).associate { it to mutableListOf<Lens>() }.toMutableMap()
             val testSequence = testInput.split(",")
             println("Sum of test hashes = ${testSequence.sumOf { it.hash() }}")
-            testSequence.forEach { it.processOperation(boxes) }
+            testSequence.forEach { boxes.processOperation(it) }
 
             val focusingPower = boxes.focusingPower()
             println("Test lenses focusing power = $focusingPower")
-        }
-
+        }.let { println("Test part took $it\n") }
 
         with(input.trim().split(",")) {
             val boxes: MutableMap<Int, MutableList<Lens>> =
-                (0..MODULO_FACTOR).associate { it to mutableListOf<Lens>() }.toMutableMap()
+                (0..< MODULO_FACTOR).associate { it to mutableListOf<Lens>() }.toMutableMap()
 
             measureTime {
                 println("Sum of hashes = ${sumOf { it.hash() }}")
             }.let { println("Part 1 took $it\n") }
 
             measureTime {
-                forEach { it.processOperation(boxes) }
-                boxes.filter { it.value.isNotEmpty() }.forEach { println(it) }
+                forEach { boxes.processOperation(it) }
                 val focusingPower = boxes.focusingPower()
                 println("Focusing power = $focusingPower")
             }.let { println("Part 2 took $it\n") }
@@ -48,12 +45,12 @@ object Day15: AocTask() {
                 }.sum()
             }.sum()
 
-    private fun String.processOperation(boxes: MutableMap<Int, MutableList<Lens>>) {
+    private fun MutableMap<Int, MutableList<Lens>>.processOperation(operation: String) {
         when {
-            contains('=') -> {
-                val (label, focalLength) = split('=')
+            operation.contains('=') -> {
+                val (label, focalLength) = operation.split('=')
                 val boxIndex = label.hash()
-                val box = boxes.getOrDefault(boxIndex, mutableListOf())
+                val box = getOrDefault(boxIndex, mutableListOf())
                 val lens = Lens(label, focalLength.toInt())
                 val replacementIndex = box.indexOfFirst { it.label == label }
                 if (replacementIndex != -1)
@@ -61,10 +58,10 @@ object Day15: AocTask() {
                 else box.add(lens)
             }
 
-            contains('-') -> {
-                val (label, _ ) = split('-')
+            operation.contains('-') -> {
+                val (label, _ ) = operation.split('-')
                 val boxIndex = label.hash()
-                val box = boxes.getOrDefault(boxIndex, mutableListOf())
+                val box = getOrDefault(boxIndex, mutableListOf())
                 val removalIndex = box.indexOfFirst { it.label == label }
                 if (removalIndex != -1)
                     box.removeAt(removalIndex)
@@ -72,13 +69,8 @@ object Day15: AocTask() {
         }
     }
 
-    private fun Char.hashStep(currentValue: Int): Int {
-        var value = currentValue
-        value += code
-        value *= MULTIPLICATION_FACTOR
-        value = value % MODULO_FACTOR
-        return value
-    }
+    private fun Char.hashStep(currentValue: Int): Int =
+        ((currentValue + code) * MULTIPLICATION_FACTOR) % MODULO_FACTOR
 
     private fun String.hash(): Int = fold(0) { acc, char -> char.hashStep(acc) }
 }
