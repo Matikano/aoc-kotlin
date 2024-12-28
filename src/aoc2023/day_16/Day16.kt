c11d4220a03767aacb0c105a0eb74d9dc7bbd8c7
Matikano <56170622+Matikano@users.noreply.github.com>
2024-12-28 18:03:02 +0100

    Aoc2023 Day 16

---
+++ src/aoc2023/day_16/Day16.kt
@@ -0,0 +1,30 @@
+package aoc2023.day_16
+
+import utils.AocTask
+import utils.models.Grid.Companion.toGrid
+import kotlin.time.measureTime
+
+object Day16: AocTask() {
+
+    override fun executeTask() {
+        measureTime {
+            val testMaze = Maze(testInput.toGrid())
+            val energizedMap = testMaze.solve()
+            println("Number of energized test positions = ${energizedMap.size}")
+
+            println()
+            println("Best energization = ${testMaze.bestEnergized}")
+        }.let { println("Test part took $it\n") }
+
+        measureTime {
+            val maze = Maze(input.toGrid())
+            val energizedMap = maze.solve()
+            println("Number of energized positions = ${energizedMap.size}")
+        }.let { println("Part 1 took $it\n") }
+
+        measureTime {
+            val maze = Maze(input.toGrid())
+            println("Best energization = ${maze.bestEnergized}")
+        }.let { println("Part 2 took $it\n") }
+    }
+}
---
+++ src/aoc2023/day_16/Maze.kt
@@ -0,0 +1,121 @@
+package aoc2023.day_16
+
+import utils.models.Direction
+import utils.models.Grid
+import utils.models.Node
+import utils.models.NodeState
+import utils.models.Position
+
+data class Maze(
+    val grid: Grid<Char>
+) {
+    val startingNodeState: NodeState = Position(0, 0) to Direction.RIGHT
+
+    val possibleStartingStates: List<NodeState> by lazy {
+        buildList {
+            grid.cells.forEach {
+                val position = it.position
+                if (position.rowIndex in 0..<grid.width && position.colIndex == 0)
+                    add(position to Direction.RIGHT)
+                if (position.rowIndex in 0..<grid.width && position.colIndex == grid.width - 1)
+                    add(position to Direction.LEFT)
+                if (position.colIndex in 0..<grid.height && position.rowIndex == 0)
+                    add(position to Direction.DOWN)
+                if (position.colIndex in 0..<grid.height && position.rowIndex == grid.height - 1)
+                    add(position to Direction.UP)
+            }
+        }
+    }
+
+    val startingStatesToScores: Map<NodeState, Int> by lazy {
+        possibleStartingStates.associate { start ->
+            start to solve(start).size
+        }
+    }
+
+    val bestEnergized: Int by lazy {
+        startingStatesToScores.values.max()
+    }
+
+    fun solve(startState: NodeState = startingNodeState): Set<Position> {
+        val startingNode = Node(
+            startState.first,
+            startState.second,
+            0
+        )
+        val queue = ArrayDeque<Node>().apply {
+            add(startingNode)
+        }
+        val energized = mutableSetOf<Position>(startingNode.position)
+        val seenStates = mutableSetOf<NodeState>()
+
+        while (queue.isNotEmpty()) {
+            val current = queue.removeFirst()
+
+            if (current.position !in energized)
+                energized.add(current.position)
+
+            if (current.state !in seenStates)
+                seenStates.add(current.state)
+
+            val field = grid[current.position]!!.value
+            val directions = field.nextDirections(current.direction)
+            val possibleNodeStates = directions.map { dir ->
+                current.position + dir to dir
+            }.filter { (pos, _ ) -> grid.isInBounds(pos) }
+                .filter { possibleNewState -> possibleNewState !in seenStates }
+
+            possibleNodeStates.forEach { newNodeState ->
+                queue.add(
+                    Node(
+                        position = newNodeState.first,
+                        direction = newNodeState.second,
+                        cost = 0
+                    )
+                )
+            }
+        }
+
+        return energized.toSet()
+    }
+
+    companion object {
+        private const val EMPTY = '.'
+        private const val SLASH_MIRROR = '/'
+        private const val BACKSLASH_MIRROR = '\\'
+        private const val HORIZONTAL_SPLITTER = '-'
+        private const val VERTICAL_SPLITTER = '|'
+
+        private fun Char.nextDirections(approachingDirection: Direction): List<Direction> = buildList {
+            when(this@nextDirections) {
+                EMPTY -> add(approachingDirection)
+                SLASH_MIRROR -> when (approachingDirection) {
+                    Direction.UP -> Direction.RIGHT
+                    Direction.RIGHT -> Direction.UP
+                    Direction.DOWN -> Direction.LEFT
+                    Direction.LEFT -> Direction.DOWN
+                    Direction.NONE -> Direction.NONE
+                }.let { add(it) }
+                BACKSLASH_MIRROR -> when (approachingDirection) {
+                    Direction.UP -> Direction.LEFT
+                    Direction.RIGHT -> Direction.DOWN
+                    Direction.DOWN -> Direction.RIGHT
+                    Direction.LEFT -> Direction.UP
+                    Direction.NONE -> Direction.NONE
+                }.let { add(it) }
+                HORIZONTAL_SPLITTER -> when (approachingDirection) {
+                    Direction.UP,
+                    Direction.DOWN -> listOf(Direction.RIGHT, Direction.LEFT)
+                    else -> listOf(approachingDirection)
+                }.let { addAll(it) }
+                VERTICAL_SPLITTER -> when (approachingDirection) {
+                    Direction.RIGHT,
+                    Direction.LEFT -> listOf(Direction.UP, Direction.DOWN)
+                    else -> listOf(approachingDirection)
+                }.let { addAll(it) }
+            }
+        }
+    }
+
+
+}
