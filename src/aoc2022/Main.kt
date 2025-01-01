386169cc93b9e1498e9502c0a32a45c6cd26b662
Matikano <56170622+Matikano@users.noreply.github.com>
2025-01-01 11:06:06 +0100

    Aoc2022 Day3

--- src/aoc2022/Main.kt
+++ src/aoc2022/Main.kt
@@ -2,8 +2,10 @@ package aoc2022
 
 import aoc2022.day_01.Day1
 import aoc2022.day_02.Day2
+import aoc2022.day_03.Day3
 
 fun main() {
 //    Day1.solve()
-    Day2.solve()
+//    Day2.solve()
+    Day3.solve()
 }
---
+++ src/aoc2022/day_03/Day3.kt
@@ -0,0 +1,60 @@
+package aoc2022.day_03
+
+import utils.AocTask
+import kotlin.time.measureTime
+
+typealias Compartments = Pair<String, String>
+typealias Group = Triple<String, String, String>
+
+object Day3: AocTask() {
+
+    private const val CHARACTERS = "abcdefghijklmnopqrstuvwxyz"
+    private const val GROUP_SIZE = 3
+
+    private val priorities: Map<Char, Int> = "$CHARACTERS${CHARACTERS.uppercase()}"
+        .mapIndexed { index, char ->
+            char to index + 1
+        }.toMap()
+
+    override fun executeTask() {
+        measureTime {
+            val backpacks = testInput.toBackpackCompartmentsList()
+            println("Sum of priorities = ${backpacks.sumOf { it.mutualItemPriority() }}")
+
+            val backpackGroups = testInput.toBackpackGroups()
+            println("Sum group label priorities = ${backpackGroups.sumOf { it.mutualItemPriority() }}")
+        }.let { println("Test part took $it\n") }
+
+        measureTime {
+            val backpacks = input.toBackpackCompartmentsList()
+            println("Sum of priorities = ${backpacks.sumOf { it.mutualItemPriority() }}")
+        }.let { println("Part 1 took $it\n") }
+
+        measureTime {
+            val backpackGroups = input.toBackpackGroups()
+            println("Sum of priorities = ${backpackGroups.sumOf { it.mutualItemPriority() }}")
+        }.let { println("Part 2 took $it\n") }
+    }
+
+    private fun String.toBackpackCompartmentsList(): List<Compartments> = lines().map { it.toBackpackCompartments() }
+
+    private fun String.toBackpackCompartments(): Compartments {
+        val (first, second) = windowed(size = length / 2, step = length / 2)
+        return first to second
+    }
+
+    private fun String.toBackpackGroups(): List<Group> =
+        lines().windowed(
+            size = GROUP_SIZE,
+            step = GROUP_SIZE
+        ) { (a, b, c) ->
+            Triple(a, b, c)
+        }
+
+    private fun Compartments.mutualItemPriority(): Int =
+        priorities[first.first { it in second }] ?: 0
+
+    private fun Group.mutualItemPriority(): Int =
+        priorities[first.first { it in second && it in third }] ?: 0
+
+}
