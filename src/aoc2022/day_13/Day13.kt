2112e9bc855af4b4a782459d016d2b30d336c6c1
Matikano <56170622+Matikano@users.noreply.github.com>
2025-01-05 10:17:47 +0100

    Day13 cleaned-up

--- src/aoc2022/day_13/Day13.kt
+++ src/aoc2022/day_13/Day13.kt
@@ -1,12 +1,12 @@
-@file:Suppress("UNCHECKED_CAST")
-
 package aoc2022.day_13
 
 import utils.AocTask
 import java.util.Comparator
 import kotlin.time.measureTime
 
-object Day13: AocTask() {
+typealias NestedListsPair = Pair<List<Any>, List<Any>>
+
+object Day13 : AocTask() {
 
     private val two = "[[2]]".parseToNestedLists()
     private val six = "[[6]]".parseToNestedLists()
@@ -23,15 +23,50 @@ object Day13: AocTask() {
         }.let { println("Test part took $it\n") }
 
         measureTime {
-            val pairs = (input.toNestedLists() + listOf(two) + listOf(six)).sortedWith(NestedListComparator)
-            println("Product of indices of divider packages = ${(pairs.indexOf(two) + 1) * (pairs.indexOf(six) + 1)}")
+            println(
+                "Product of indices of divider packages = ${
+                    input.toNestedLists().productOfDividerPackageIndices()
+                }"
+            )
+        }.let { println("Part 2 took $it\n") }
+
+        measureTime {
+            println(
+                "Product of indices of divider packages iteratevly = ${
+                    input.toNestedLists().productOfDividerPackageIndicesIter()
+                }"
+            )
         }.let { println("Part 2 took $it\n") }
     }
 
-    private fun List<Pair<List<Any>, List<Any>>>.orderedIndicesSum(): Int =
+    private fun List<List<Any>>.productOfDividerPackageIndicesIter(): Int {
+        // We index elements starting from 1, and we know that [[6]] > [[2]],
+        // so it is always going to be after two in sorted list
+        var i2 = 1
+        var i6 = 2
+
+        forEach { element ->
+            if (element.compareTo(two) < 0) {
+                i2++
+                i6++
+            } else if (element.compareTo(six) < 0) {
+                i6++
+            }
+        }
+
+        return i2 * i6
+    }
+
+    private fun List<List<Any>>.productOfDividerPackageIndices(): Int =
+        (this + listOf(two, six))
+            .sortedWith(NestedListComparator)
+            .let { sorted ->
+                (sorted.indexOf(two) + 1) * (sorted.indexOf(six) + 1)
+            }
+
+    private fun List<NestedListsPair>.orderedIndicesSum(): Int =
         mapIndexed { index, (first, second) ->
-            if (first.compareTo(second) < 1)
-                index + 1
+            if (first.compareTo(second) < 1) index + 1
             else 0
         }.sum()
 
@@ -46,6 +81,7 @@ object Day13: AocTask() {
             first.parseToNestedLists() to second.parseToNestedLists()
         }
 
+    @Suppress("UNCHECKED_CAST")
     private fun String.parseToNestedLists(): List<Any> {
         val stack = mutableListOf<MutableList<Any>>()
         var currentList = mutableListOf<Any>()
@@ -57,6 +93,7 @@ object Day13: AocTask() {
                     stack.add(currentList)
                     currentList = mutableListOf()
                 }
+
                 ']' -> {
                     if (stack.isNotEmpty()) {
                         val parentList = stack.removeLast()
@@ -66,6 +103,7 @@ object Day13: AocTask() {
                         return currentList
                     }
                 }
+
                 in '0'..'9' -> {
                     val numStart = index
                     while (index < length && this[index].isDigit()) {
@@ -82,30 +120,40 @@ object Day13: AocTask() {
     }
 
 
-    object NestedListComparator: Comparator<Any> {
+    object NestedListComparator : Comparator<Any> {
         override fun compare(o1: Any, o2: Any): Int = o1.compareTo(o2)
     }
 
-    fun Any.compareTo(other: Any): Int {
-        return if (this is Int) {
-            when (other) {
-                is Int -> this - other
-                is List<*> -> {
-                    if (other.isEmpty()) 1
-                    else listOf(this).compareTo(other)
+    fun Any.compareTo(other: Any): Int =
+        when (this) {
+            is Int -> {
+                when (other) {
+                    is Int -> this - other
+                    is List<*> -> {
+                        if (other.isEmpty()) 1
+                        else listOf(this).compareTo(other)
+                    }
+
+                    else -> throw IllegalStateException("Unsupported type")
                 }
-                else -> throw IllegalStateException("Unsupported type")
             }
-        } else if (this is List<*>) {
-            if (other is Int) compareTo(listOf(other))
-            else if (other is List<*>) {
-                zip(other).forEach { (a, b) ->
-                    val comparison = a!!.compareTo(b!!)
-                    if (comparison != 0)
-                        return comparison
+
+            is List<*> -> {
+                when (other) {
+                    is Int -> compareTo(listOf(other))
+                    is List<*> -> {
+                        zip(other).forEach { (a, b) ->
+                            val comparison = a!!.compareTo(b!!)
+                            if (comparison != 0)
+                                return comparison
+                        }
+                        size - other.size
+                    }
+
+                    else -> throw IllegalStateException("Unsupported type")
                 }
-                size - other.size
-            } else throw IllegalStateException("Unsupported type")
-        } else throw IllegalStateException("Unsupported type")
-    }
+            }
+
+            else -> throw IllegalStateException("Unsupported type")
+        }
 }
