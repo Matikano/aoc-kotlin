be3744ee2963d6c6b8825c3fcda7d690d5c5954e
Matikano <56170622+Matikano@users.noreply.github.com>
2025-02-05 18:41:42 +0100

    Aoc 2021 Day 21

--- src/aoc2021/day_21/Player.kt
+++ src/aoc2021/day_21/Player.kt
@@ -4,26 +4,6 @@ data class Player(
     var position: Int,
     var score: Int = 0
 ) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Player
-
-        if (position != other.position) return false
-        if (score != other.score) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = position
-        result = 31 * result + score
-        return result
-    }
-
-    fun copy(): Player = Player(position, score)
-
     fun takeATurn(rollSum: Int) {
         position = (position - 1 + rollSum) % POSITIONS.size + 1
         score += position
